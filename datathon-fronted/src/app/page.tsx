d5f50d317ee8084ede23059d92bf6d334e18b6bf
David Amouzou <davidamzou@gmail.com>
2025-02-13 14:01:34 +0100

    init front

--- datathon-fronted/src/app/page.tsx
+++ datathon-fronted/src/app/page.tsx
@@ -7,7 +7,8 @@ export default function Home() {
         <div className="flex flex-col items-center justify-center w-3/4 h-full relative overflow-hidden">
             <div className="flex flex-col items-center justify-center p-24">
                 <Image className={"w-16 h-16"} src={robot} alt="robot" width={200} height={200} />
-              <h1 className={"font-black text-4xl mt-4"}>Assistant Datathon</h1>
+              <h1 className={"font-black text-4xl mt-4"}>Bonjour</h1>
+                <p className={"mt-2 text-black/70"}>Je suis votre assistant support, je vous aide à trouver les réponses à vos questions.</p>
             </div>
           <div className={"w-3/4 bg-white fixed bottom-0 p-5"}>
             <textarea
