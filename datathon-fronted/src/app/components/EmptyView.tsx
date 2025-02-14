6c1ad856dc3ffcb1b886e4b0d975f51012dc1169
David Amouzou <davidamzou@gmail.com>
2025-02-14 11:46:18 +0100

    feat: update logo format in EmptyView component and adjust image container styling

--- datathon-fronted/src/app/components/EmptyView.tsx
+++ datathon-fronted/src/app/components/EmptyView.tsx
@@ -1,17 +1,20 @@
 import Image from "next/image";
-import logo from "@/app/assets/logo.jpeg";
+import logo from "@/app/assets/logo.webp";
 
 // Composant pour afficher le contenu initial lorsque la conversation est vide
 export const EmptyContent = () => {
     return (
         <div className="flex flex-col items-center justify-center p-24">
-            <Image
-                className="w-48 h-48 object-cover"
-                src={logo}
-                alt="robot"
-                width={200}
-                height={200}
-            />
+            <div className="w-96 h-48">
+                <Image
+                    className="object-cover w-full h-full"
+                    src={logo}
+                    alt="robot"
+                    width={200}
+                    height={200}
+                />
+            </div>
+
             <h1 className="font-black text-4xl mt-20">Bonjour</h1>
             <p className="mt-2 text-black/70">
                 Je suis votre assistant support, je vous aide à trouver les réponses à
